4fd8a3823e7414273f586684d28054b43729b29c
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-06-13 14:45:49 -0500

    90% complete

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -5,11 +5,11 @@ function Footer() {
                 <div className="container">
                     <h3>Olayinka Kolawole</h3>
                     <div className="socials">
-                        <a href="" className="socials-link"><i className='bx bxl-twitter'></i></a>
-                        <a href="" className="socials-link"><i className='bx bxl-facebook'></i></a>
-                        <a href="" className="socials-link"><i className='bx bxl-linkedin'></i></a>
-                        <a href="" className="socials-link"><i className='bx bxl-meta'></i></a>
-                        <a href="" className="socials-link"><i className='bx bxl-github'></i></a>
+                        <a href="https://" className="socials-link"><i className='bx bxl-twitter'></i></a>
+                        <a href="https://" className="socials-link"><i className='bx bxl-facebook'></i></a>
+                        <a href="https://" className="socials-link"><i className='bx bxl-linkedin'></i></a>
+                        <a href="https://" className="socials-link"><i className='bx bxl-meta'></i></a>
+                        <a href="https://" className="socials-link"><i className='bx bxl-github'></i></a>
                     </div>
                 </div>
             </footer>
